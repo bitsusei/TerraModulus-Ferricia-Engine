ab4591ffccb5603c3e5446a40fe9694096d5df05
Ben Forge <74168521+BenCheung0422@users.noreply.github.com>
2026-09-20 09:08:53 +0800

    MUI: Change CameraSpace intersection algorithm
    From Mesh::intersection to custom SAT

--- src/math.rs
+++ src/math.rs
@@ -2,8 +2,11 @@
  * SPDX-FileCopyrightText: 2026 TerraModulus Team and Contributors
  * SPDX-License-Identifier: LGPL-3.0-only
  */
+use bytemuck::cast_slice;
+use csgrs::float_types::parry3d::na::Point3;
 use csgrs::mesh::Mesh;
 use csgrs::traits::CSG;
+use itertools::Itertools;
 use nalgebra_glm::DVec3;
 
 pub(crate) struct CameraSpace {
@@ -45,10 +48,60 @@ impl CameraSpace {
 		}
 	}
 
+	/// Intersection test by using Separating Axis Theorem (SAT).
 	pub(crate) fn intersects(&self, min: DVec3, max: DVec3) -> bool {
-		// Maybe compare performance difference between this and a test with Separating Axis Theorem (SAT)
-		let cuboid = Mesh::cuboid(max.x - min.x, max.y - min.y, max.z - min.z, None)
+		let cuboid: Mesh<()> = Mesh::cuboid(max.x - min.x, max.y - min.y, max.z - min.z, None)
 			.translate(min.x, min.y, min.z);
-		!self.parallelepiped.intersection(&cuboid).polygons.is_empty()
+		let mut axes = Vec::<DVec3>::new();
+		axes.extend(self.parallelepiped.polygons
+			.iter()
+			.map(|f| f.plane.normal())
+			.unique_by(|v| cast_slice::<_, u8>(v.as_slice()).to_vec()));
+		axes.extend(cuboid.polygons
+			.iter()
+			.map(|f| f.plane.normal())
+			.unique_by(|v| cast_slice::<_, u8>(v.as_slice()).to_vec()));
+		for ea in self.parallelepiped.polygons
+			.iter()
+			.flat_map(|f| f.edges().map(|e| (e.1.pos - e.0.pos).normalize()))
+			.unique_by(|v| cast_slice::<_, u8>(v.as_slice()).to_vec()) {
+			for eb in cuboid.polygons
+				.iter()
+				.flat_map(|f| f.edges().map(|e| (e.1.pos - e.0.pos).normalize()))
+				.unique_by(|v| cast_slice::<_, u8>(v.as_slice()).to_vec()) {
+				let axis = ea.cross(&eb);
+				if axis.norm() > f64::EPSILON {
+					axes.push(axis.normalize());
+				}
+			}
+		}
+		fn range_projection(vertices: &[Point3<f64>], axis: DVec3) -> (f64, f64) {
+			let mut min_proj = None;
+			let mut max_proj = None;
+			for vertex in vertices {
+				let proj = vertex.coords.dot(&axis);
+				match min_proj {
+					None => min_proj = Some(proj),
+					Some(min) if proj < min => min_proj = Some(proj),
+					_ => {}
+				}
+				match max_proj {
+					None => max_proj = Some(proj),
+					Some(max) if proj > max => max_proj = Some(proj),
+					_ => {}
+				}
+			}
+			(min_proj.unwrap(), max_proj.unwrap())
+		}
+		let a_vertices = self.parallelepiped.vertices().iter().map(|v| v.pos).collect::<Vec<_>>();
+		let b_vertices = cuboid.vertices().iter().map(|v| v.pos).collect::<Vec<_>>();
+		for axis in axes {
+			let a = range_projection(&a_vertices, axis);
+			let b = range_projection(&b_vertices, axis);
+			if a.0 > b.1 || a.1 < b.0 {
+				return false; // gap exists thus no intersection
+			}
+		}
+		true // gap not found this intersection
 	}
 }
