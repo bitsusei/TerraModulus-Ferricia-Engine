883cc1e7f03e995ac6c6c30b9ee16793f274704b
Ben Forge <74168521+BenCheung0422@users.noreply.github.com>
2026-09-25 15:27:35 +0800

    Phy: Optimize API with composite body

--- src/lib.rs
+++ src/lib.rs
@@ -1331,25 +1331,6 @@ jni_ferricia! {
 }
 
 jni_ferricia! {
-	Physics.setPhyGeomOffsetPos(mut env: JNIEnv, class: JClass, handle: jlong, data: jdoubleArray) {
-		jni_get_arr!(arr = JDoubleArray; data, env);
-		unsafe { jni_ref_ptr::<PhyRawGeomPlaceable>(handle).set_offset_position(arr[0], arr[1], arr[2]) }
-	}
-}
-
-jni_ferricia! {
-	Physics.getPhyGeomPos(mut env: JNIEnv, class: JClass, handle: jlong) -> jdoubleArray {
-		let geom = jni_ref_ptr::<PhyRawGeomPlaceable>(handle);
-		let r0 = unsafe { geom.get_position() };
-		let r1 = unsafe { geom.get_offset_position() };
-		let arr = env.new_double_array(3).expect("Cannot create Java double array");
-		let r = r0.iter().zip(r1.iter()).map(|(a, b)| *a + *b).collect::<Box<_>>();
-		env.set_double_array_region(&arr, 0, &r).expect("Cannot set Java double array");
-		arr.into_raw()
-	}
-}
-
-jni_ferricia! {
 	Physics.getPhyBodyPos(mut env: JNIEnv, class: JClass, handle: jlong) -> jdoubleArray {
 		let r = unsafe { jni_ref_ptr::<PhyBody>(handle).get_position() };
 		let arr = env.new_double_array(3).expect("Cannot create Java double array");
@@ -1431,6 +1412,13 @@ jni_ferricia! {
 		let geom = jni_ref_ptr::<PhyRawGeom<OdePlaceableMarker>>(handle);
 		geom.set_category_bits(arr[0] as _);
 		geom.set_collide_bits(arr[1] as _);
+	}
+}
+
+jni_ferricia! {
+	Physics.setPhyRawGeomPlaceableOffsetPos(mut env: JNIEnv, class: JClass, handle: jlong, data: jdoubleArray) {
+		jni_get_arr!(arr = JDoubleArray; data, env);
+		unsafe { jni_ref_ptr::<PhyRawGeomPlaceable>(handle).set_offset_position(arr[0], arr[1], arr[2]) }
 	}
 }
 
